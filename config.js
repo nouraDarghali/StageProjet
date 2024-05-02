785eddb1d7188dcfcd31f6712fb5104f38e54635
@ibtissam_bcn <147552959+ibtissambcn@users.noreply.github.com>
2024-05-02 18:41:43 +0100

    testDB
    le dossier config  {   1 fichier config.js
                                    2 fichier test-mongodb-cnnection.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// app/config/config.js
+
+const config = {
+    mongoURI: 'mongodb+srv://assyaasbabou:Hxj2FQ1ismuigA7J@cluster0.9sqdpz0.mongodb.net/app',
+    //mongoURI: 'mongodb+srv://assyaasbab@cluster0.9sqdpz0.mongodb.net/app',
+    options: { useNewUrlParser: true, useUnifiedTopology: true } // Ajoutez d'autres options si nécessaire
+  };
+  
+  module.exports = config;
+  
