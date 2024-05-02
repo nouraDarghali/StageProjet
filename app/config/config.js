e6a951a7a688c43a40993e00821ddc613b5f50cf
nouraDarghali <noradr456@gmail.com>
2024-05-02 23:28:07 +0100

    commit

--- app/config/config.js
+++ app/config/config.js
@@ -1,7 +1,7 @@
 // app/config/config.js
 
 const config = {
-    mongoURI: 'mongodb+srv://noura:123%40%2FA@cluster0.ndp8imb.mongodb.net/crud_db',
+    mongoURI: 'mongodb+srv://assyaasbabou:Hxj2FQ1ismuigA7J@cluster0.9sqdpz0.mongodb.net/app',
     //mongoURI: 'mongodb+srv://assyaasbab@cluster0.9sqdpz0.mongodb.net/app',
     
   };
